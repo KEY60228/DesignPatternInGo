fe1dfa80817b6f2330e339d075f3e35d9ee57c84
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-08-28 13:44:17 +0900

    exercise-1

--- Bridge/main.go
+++ Bridge/main.go
@@ -4,9 +4,11 @@ func main() {
 	d1 := NewDisplay(NewStringDisplayImpl("Hello, Japan."))
 	d2 := NewCountDisplay(NewStringDisplayImpl("Hello, World."))
 	d3 := NewCountDisplay(NewStringDisplayImpl("Hello, Universe."))
+	d4 := NewRandomDisplay(NewStringDisplayImpl("Random Display"))
 
 	d1.Do()
 	d2.Do()
 	d3.Do()
 	d3.MultiDisplay(5)
+	d4.RandomDo(100)
 }
