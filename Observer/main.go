988a53c8e0603446d2bc78bb216664f3b318b0cb
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-10-01 15:01:31 +0900

    exercise-2

--- Observer/main.go
+++ Observer/main.go
@@ -4,7 +4,9 @@ func main() {
 	generator := NewIncrementalNumberGenerator(10, 50, 5)
 	observer1 := NewDigitObserver()
 	observer2 := NewGraphObserver()
+	observer3 := NewPlusObserver()
 	generator.AddObserver(observer1)
 	generator.AddObserver(observer2)
+	generator.AddObserver(observer3)
 	generator.Execute()
 }
