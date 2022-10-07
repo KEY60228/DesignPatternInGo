75467c730f24d9218f883559c0f2e8ef74066f60
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-10-07 22:21:58 +0900

    exercise

--- Memento/main.go
+++ Memento/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
+const filename = "game.dat"
+
 func main() {
-	gamer := NewGamer(100)
-	memento := gamer.CreateMemento()
+	gamer := &Gamer{}
+	memento := &Memento{}
+
+	_, err := os.Stat(filename)
+	if err != nil {
+		gamer = NewGamer(100)
+		memento = gamer.CreateMemento()
+	} else {
+		memento = memento.LoadFromFile(filename)
+		gamer.RestoreMemento(memento)
+	}
 
 	for i := 0; i < 100; i++ {
 		fmt.Printf("=== %d\n", i)
@@ -20,6 +32,7 @@ func main() {
 		if gamer.GetMoney() > memento.GetMoney() {
 			fmt.Println("「だいぶ増えたし現在の状態を保存しておこう…」")
 			memento = gamer.CreateMemento()
+			memento.SaveToFile(filename, memento)
 		} else if gamer.GetMoney() < memento.GetMoney()/2 {
 			fmt.Println("「だいぶ減ったから前の状態まで戻そう！」")
 			gamer.RestoreMemento(memento)
--- Memento/memento.go
+++ Memento/memento.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
 type Memento struct {
 	money  int
 	fruits []string
@@ -12,6 +20,28 @@ func NewMemento(money int) *Memento {
 	}
 }
 
+func (m Memento) MarshalJSON() ([]byte, error) {
+	v, err := json.Marshal(&struct {
+		Money  int      `json:"money"`
+		Fruits []string `json:"fruits"`
+	}{
+		Money:  m.money,
+		Fruits: m.fruits,
+	})
+	return v, err
+}
+
+func (m *Memento) UnmarshalJSON(b []byte) error {
+	m2 := &struct {
+		Money  int      `json:"money"`
+		Fruits []string `json:"fruits"`
+	}{}
+	err := json.Unmarshal(b, m2)
+	m.money = m2.Money
+	m.fruits = m2.Fruits
+	return err
+}
+
 func (m *Memento) GetMoney() int {
 	return m.money
 }
@@ -22,4 +52,47 @@ func (m *Memento) AddFruit(fruit string) {
 
 func (m *Memento) GetFruits() []string {
 	return m.fruits
+}
+
+func (m *Memento) SaveToFile(filename string, memento *Memento) bool {
+	f, err := os.Create(filename)
+	if err != nil {
+		return false
+	}
+	defer f.Close()
+
+	j, err := json.Marshal(memento)
+	if err != nil {
+		return false
+	}
+
+	if _, err := fmt.Fprint(f, string(j)); err != nil {
+		return false
+	}
+
+	return true
+}
+
+func (m *Memento) LoadFromFile(filename string) *Memento {
+	var memento Memento
+
+	f, err := os.Open(filename)
+	if err != nil {
+		// return nil
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	j, err := io.ReadAll(f)
+	if err != nil {
+		// return nil
+		log.Fatal(err)
+	}
+
+	if err := json.Unmarshal(j, &memento); err != nil {
+		// return nil
+		log.Fatal(err)
+	}
+
+	return &memento
 }
